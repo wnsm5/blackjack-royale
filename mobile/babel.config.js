73eed21172108c589897c09e4ef322f5fb7dff8e
Ewen Verdin <ewenverdin@mac.home>
2026-08-14 21:52:03 +0200

    fix(mobile): add inline DOMRect polyfill and clean babel config for error-free Expo 54 execution

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,10 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      '@babel/plugin-transform-private-methods',
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-property-in-object',
-    ],
   };
 };
