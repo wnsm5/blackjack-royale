b9683f80bf2dfc8a541beaaa6e93bc98bf046f74
Ewen Verdin <ewenverdin@mac.home>
2026-08-14 21:57:31 +0200

    fix(mobile): add DOMRectReadOnly polyfill to Metro early injection

--- mobile/polyfill.js
+++ mobile/polyfill.js
@@ -1,4 +1,4 @@
-// EARLY POLYFILL SCRIPT - Loaded by Metro before CommonJS runtime / module.exports is setup
+// EARLY POLYFILL SCRIPT - Loaded by Metro before setupDOM / module initialization
 (function () {
   function DOMRectPolyfill(x, y, w, h) {
     this.x = Number(x) || 0;
@@ -17,22 +17,45 @@
     return { x: this.x, y: this.y, width: this.width, height: this.height, top: this.top, right: this.right, bottom: this.bottom, left: this.left };
   };
 
-  var target = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 'undefined' ? global : typeof window !== 'undefined' ? window : this;
+  function DOMRectReadOnlyPolyfill(x, y, w, h) {
+    return new DOMRectPolyfill(x, y, w, h);
+  }
+  DOMRectReadOnlyPolyfill.fromRect = DOMRectPolyfill.fromRect;
+  DOMRectReadOnlyPolyfill.prototype = DOMRectPolyfill.prototype;
 
-  if (target && typeof target.DOMRect === 'undefined') {
-    try {
-      Object.defineProperty(target, 'DOMRect', {
-        value: DOMRectPolyfill,
-        writable: true,
-        configurable: true,
-        enumerable: false,
-      });
-    } catch (e) {
-      target.DOMRect = DOMRectPolyfill;
+  var targets = [
+    typeof globalThis !== 'undefined' ? globalThis : null,
+    typeof global !== 'undefined' ? global : null,
+    typeof window !== 'undefined' ? window : null,
+  ];
+
+  targets.forEach(function (target) {
+    if (!target) return;
+
+    if (typeof target.DOMRect === 'undefined') {
+      try {
+        Object.defineProperty(target, 'DOMRect', {
+          value: DOMRectPolyfill,
+          writable: true,
+          configurable: true,
+          enumerable: false,
+        });
+      } catch (e) {
+        target.DOMRect = DOMRectPolyfill;
+      }
     }
-  }
 
-  if (typeof global !== 'undefined' && typeof global.DOMRect === 'undefined') {
-    global.DOMRect = DOMRectPolyfill;
-  }
+    if (typeof target.DOMRectReadOnly === 'undefined') {
+      try {
+        Object.defineProperty(target, 'DOMRectReadOnly', {
+          value: DOMRectReadOnlyPolyfill,
+          writable: true,
+          configurable: true,
+          enumerable: false,
+        });
+      } catch (e) {
+        target.DOMRectReadOnly = DOMRectReadOnlyPolyfill;
+      }
+    }
+  });
 })();
